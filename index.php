96df5e59e0cee56c27407484d30daac2441af776
Gabriel Argel <leirbag9710@gmail.com>
2019-02-12 23:35:07 -0500

    Primer commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>VAP</title>
+  </head>
+  <body>
+    <h1 class="hprime">Animado entre Páginas</h1>
+    <hr>
+    <section  class="primero">
+      <p>
+        Con este documento se pretende expandir la forma de ver estos tres medios (Libros, aanime y manga)
+        <br> darle una nueva idea de lo que también pueden ser, claro, desde nuestra opinión.
+        <br>¿Y por qué de estos tres? Se estarán preguntando, y la respuesta es que, estos son los que más
+        <br>nos gustan y los que más hemos explorado hasta cierto grado de profundidad.
+      </p>
+      <p>
+        Nuestro título refleja a la perfeciión lo anterior dicho, ¿de qué manera? pues, animado entre Páginas
+        <br>es ver como nuestros cuentos favoritos, historias de los autores que amamos, pueden ser llevadas
+        <br>de manera maravillosa a la animación. No solo una animación real, sino en nuestra cabeza también
+        <br>Y también como series animadas pueden tener un valor tan alto como los libros, en sus historias
+        <br>en sus personajes, en los conflictos que allí se tratan, allí es dnde está la magía de animado
+        <br>entre Páginas, ya que es aquí, donde nuestras imaginación y comprensión, pueden divertirse de igual manera.
+      </p>
+      <p>
+        En el caso de los libros casi siempre, por alguna razón, creen que solo es un medio super
+        <br>elevado de categoria, al cual solo unos pocos pueden acceder y siempre son de caracter intelectual
+        <br>y ciertamente muchos lo son, pero estos también está diseñados para el ocio, diversión y recreaión
+        <br>personal, y cómo esto sigue haciendo de ellos una gradioa obra.
+      </p>
+      <p>
+
+      </p>
+    </section>
+  </body>
+</html>
